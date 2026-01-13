a194e31be000d9c73e698100700b5a9e41eac075
Saumya Barman <barmansaumya26@gmail.com>
2026-01-13 13:00:05 +1100

    Changes to be committed: 	new file:   ._.git 	new file:   ._index.html 	new file:   ._script.js 	new file:   ._style.css 	new file:   index.html 	new file:   script.js 	new file:   style.css

---
+++ script.js
@@ -0,0 +1,57 @@
+const waffle = document.getElementById("waffle");
+const goodButton = document.getElementById("yesButton");
+const badButton = document.getElementById("noButton");
+
+yesButton.addEventListener("click", () => {
+    ScoreUpdate(True);
+})
+
+noButton.addEventListener("click", () => {
+    ScoreUpdate(False);
+})
+
+const emojis = ["🧇", "🥞", "🦄", "🥞", "🍯", "💩", "🐸"];
+let score = 0;
+
+function showRandomEmoji() {
+    const randomIndex = Math.floor(Math.random() * emojis.length);
+    return emojis[randomIndex];
+} 
+
+function showEmoji() {
+    document.getElementById("emoji").textContent = showRandomEmoji();
+}
+
+function ScoreUpdate(isWaffle) {
+    if (isWaffle) {
+        score++;
+        showTemporaryMessage(isWaffle ?"Some lucky person is getting waffles! 🎉" : "Aww, no waffles this time. 😢");
+        document.getElementById("score").textContent = "Waffles Shipped: " + score;
+        showEmoji(); 
+    } 
+
+    function showTemporaryMessage(msg) {
+        const messageDiv = document.createElement("div");
+        messageDiv.textContent = msg;
+        messageDiv.style.position = "absolute";
+        messageDiv.style.top = "50px";
+        messageDiv.style.left = "50%";
+        messageDiv.style.transform = "translate(-50%)";
+        messageDiv.style.fontSize = "24px";
+        messageDiv.style.background = "#fff8e7";
+        messageDiv.style.padding = "10px 20px";
+        messageDiv.style.borderRadius = "12px";
+        document.body.appendChild(messageDiv);
+        
+        setTimeout(() => {
+            document.body.removeChild(messageDiv);
+        }, 2000);
+    } 
+}
+
+//edits to be made
+//how to get score to update after button click??
+//gah
+
+
+
